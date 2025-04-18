2d12cbb24d08f7973e81e72f06e15ee1335cbb42
evsukov <evsukov@nsd.ru>
2025-04-18 23:14:15 +0300

    Исправление замечания про лишние вызовы рефлексии

--- hw05-cloader/homework/src/main/java/ru/nsd/cloader/infrastructure/LoggingInvocationHandler.java
+++ hw05-cloader/homework/src/main/java/ru/nsd/cloader/infrastructure/LoggingInvocationHandler.java
@@ -3,20 +3,58 @@ package ru.nsd.cloader.infrastructure;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 import ru.nsd.cloader.annotations.Log;
 
 public class LoggingInvocationHandler implements InvocationHandler {
     private final Object target;
+    private final Map<Method, Boolean> methodLoggingCache = new HashMap<>();
 
     public LoggingInvocationHandler(Object target) {
         this.target = target;
+        initializeLoggingMethods();
+    }
+
+    /**
+     * Инициализирует кэш методов, помеченных аннотацией @Log
+     */
+    private void initializeLoggingMethods() {
+        Class<?> targetClass = target.getClass();
+
+        // Собираем все публичные методы целевого класса
+        for (Method method : targetClass.getMethods()) {
+            // Проверяем наличие аннотации @Log
+            if (method.isAnnotationPresent(Log.class)) {
+                // Находим соответствующий метод интерфейса
+                try {
+                    Method interfaceMethod = findInterfaceMethod(method);
+                    methodLoggingCache.put(interfaceMethod, true);
+                } catch (NoSuchMethodException ignored) {
+                    // Метод не принадлежит интерфейсу (для JDK proxy это невозможно)
+                }
+            }
+        }
+    }
+
+    /**
+     * Находит метод интерфейса по методу целевого класса
+     */
+    private Method findInterfaceMethod(Method targetMethod) throws NoSuchMethodException {
+        for (Class<?> intf : target.getClass().getInterfaces()) {
+            try {
+                return intf.getMethod(targetMethod.getName(), targetMethod.getParameterTypes());
+            } catch (NoSuchMethodException ignored) {
+            }
+        }
+        throw new NoSuchMethodException();
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        Method targetMethod = target.getClass().getMethod(method.getName(), method.getParameterTypes());
-        if (targetMethod.isAnnotationPresent(Log.class)) {
+        // Проверяем кэш вместо поиска через рефлексию
+        if (methodLoggingCache.getOrDefault(method, false)) {
             logMethodCall(method.getName(), args);
         }
         return method.invoke(target, args);
